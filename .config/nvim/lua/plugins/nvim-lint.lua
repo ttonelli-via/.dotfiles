c46c9f6845c1005ba54f974c274b1ad88b6cb8bd
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-03-17 17:20:33 -0400

    rm none-ls and mason plugin for conform and nvim-lint with mason tool installer

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,26 @@
+return {
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    config = function()
+        local lint = require("lint")
+
+        lint.linters_by_ft = {
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            astro = { "eslint_d" },
+            python = { "ruff" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+            group = vim.api.nvim_create_augroup("Linting", { clear = true }),
+            callback = function()
+                lint.try_lint()
+            end,
+        })
+    end,
+}

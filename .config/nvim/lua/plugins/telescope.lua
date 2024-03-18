83eff5e547ee1f526168725b9222c04fa6dbdc7e
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-03-17 20:26:38 -0400

    add telescope ui select

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,22 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         branch = "0.1.x",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+            -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+            -- Only load if `make` is available. Make sure you have the system
+            -- requirements installed.
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                -- NOTE: If you are having trouble with this installation,
+                --       refer to the README for telescope-fzf-native for more instructions.
+                build = "make",
+                cond = function()
+                    return vim.fn.executable("make") == 1
+                end,
+            },
+        },
         config = function()
             require("telescope").setup({
                 defaults = {
@@ -15,7 +30,7 @@ return {
                 },
             })
 
-            -- Enable telescope fzf native, if installed
+            pcall(require("telescope").load_extension, "ui-select")
             pcall(require("telescope").load_extension, "fzf")
 
             -- See `:help telescope.builtin`
@@ -35,19 +50,6 @@ return {
             vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
             vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
             vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
-        end,
-    },
-
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-    -- Only load if `make` is available. Make sure you have the system
-    -- requirements installed.
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = "make",
-        cond = function()
-            return vim.fn.executable("make") == 1
         end,
     },
 }

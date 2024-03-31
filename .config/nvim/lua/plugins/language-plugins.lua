936b9d6903c05478a5b914be839c594c16ae85ed
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-03-31 17:20:25 -0400

    add keymaps for common rust analyzer commands

--- .config/nvim/lua/plugins/language-plugins.lua
+++ .config/nvim/lua/plugins/language-plugins.lua
@@ -1,12 +1,27 @@
 return {
     -- Rust Plugins
-    { "mrcjkb/rustaceanvim", version = "^3", ft = { "rust" } },
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^4",
+        ft = { "rust" },
+        config = function()
+            vim.g.rustaceanvim = {
+                tools = {},
+                server = {
+                    on_attach = function(_, bufnr)
+                        vim.keymap.set("n", "<leader>xp", "<CMD>RustLsp expandMacro<CR>", { buffer = bufnr, desc = "E[xp]and Macro" })
+                        vim.keymap.set("n", "<leader>rp", "<CMD>RustLsp rebuildProcMacros<CR>", { buffer = bufnr, desc = "[R]ebuild [P]roc Macros" })
+                    end,
+                },
+            }
+        end,
+    },
     {
         "saecki/crates.nvim",
         event = "BufRead Cargo.toml",
         dependencies = { { "nvim-lua/plenary.nvim" } },
         config = function()
-            require("crates").setup()
+            require("crates").setup({})
         end,
     },
 }

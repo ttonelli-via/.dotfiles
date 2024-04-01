30bbd6e436419d00220e5956763f99a63a59c322
timTonelli <102175985+timTonelli@users.noreply.github.com>
2024-03-31 21:56:49 -0400

    remap fuzzy find current buf

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -36,9 +36,11 @@ return {
             -- See `:help telescope.builtin`
             vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
             vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
-            vim.keymap.set("n", "<leader>/", function()
-                -- You can pass additional configuration to telescope to change theme, layout, etc.
-                require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            local builtin = require("telescope.builtin")
+
+            -- NOTE: this is actually <C-/> but terminal keycodes are weird
+            vim.keymap.set("n", "<C-_>", function()
+                builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
                     winblend = 10,
                     previewer = false,
                 }))
